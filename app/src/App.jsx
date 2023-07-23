a22d3bdc31b47cc902f2d192e90b00e6d19ae037
Shantanu Sutar <shantanusutar17@gmail.com>
2023-07-23 19:06:04 +0530

    implmenting basic routes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,35 @@
-function Vite() {
-  const x = 2;
-  return <div>Hello Vite!</div>;
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default Vite;
+export default App;
