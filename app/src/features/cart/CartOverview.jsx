9fda68f0df676d894cf439e815f465465c4fe0b1
Shantanu Sutar <shantanusutar17@gmail.com>
2023-08-13 20:04:30 +0530

    building the Cart Overview with Redux Selectors

--- app/src/features/cart/CartOverview.jsx
+++ app/src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getTotalCartPrice, getTotalCartQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity); // useSelector is a hook that allows you to extract data from the Redux store state, using a selector function.
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null; // If there are no items in the cart, we don't want to show the cart overview.
+
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
